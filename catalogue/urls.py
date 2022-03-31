7cbe173e3dda569dcdf466130f8c71f67c08f74a
sazamat <s.azamat0806@gmail.com>
2022-03-31 18:35:51 +0600

    displayin employee table in catalogue's page Bug: name of departnment is displaying for every row created

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.display)
- ]
+
+    path('', views.all_contacts),
+]
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
-def display(request):
-    return render(request, 'contacts.html')
+def all_contacts(request):
+    contact_list = Employee.objects.all()
+    return render(request, 'contacts.html', {'contact_list': contact_list})
+
+
+
